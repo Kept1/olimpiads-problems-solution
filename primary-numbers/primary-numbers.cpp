ae068d4a5eaf606433db62351198ee5fadc03505
Kept1 <Konnik.oleg.05@mail.ru>
2019-04-27 12:10:27 +0300

    Primary numbers

---
+++ primary-numbers/primary-numbers.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+bool is_primary(int i) {
+	for(int j = 2; j <= int(sqrt(i)); j++) {
+		if (i % j == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int n, m;
+	cin >> m >> n;
+	for (int i = m; i <= n; i++) {
+		if (is_primary(i)) {
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
